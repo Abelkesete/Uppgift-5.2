4d8848b30acaed2dc9dfe3384f9ce8f28851bda5
Pingvu <nhlpool27@gmail.com>
2023-01-20 14:50:37 +0100

    Add project files.

---
+++ Uppgift 5.2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Uppgift_5._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            
+            string [] tal = new string[3];
+            
+
+                tal[0] = "3";
+                tal[1] = "4";
+                Console.WriteLine("Skriv tredje tal: ");
+                tal[2] = Console.ReadLine();
+
+                Console.WriteLine($"{tal[0]}, {tal[1]} och {tal[2]} ");
+        
+        }
+    }
+}
